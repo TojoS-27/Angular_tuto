22041047044440cee8ec51f14c830e5060cf09d7
tojo <aud.jeanba@gmail.com>
2019-10-18 11:41:38 +0300

    Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { resolve } from "dns";
 
 @Component({
   selector: "app-root",
@@ -7,6 +8,13 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   isAuth = false;
+
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(() => {
+      resolve(date);
+    }, 2000);
+  });
 
   appareils = [
     {
