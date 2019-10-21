053dfe96fec91bed087c5209907998cff6bbce95
tojo <aud.jeanba@gmail.com>
2019-10-21 12:45:55 +0300

    Protection link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,24 @@ import { Routes, RouterModule } from "@angular/router";
 import { AppareilViewComponent } from "./appareil-view/appareil-view.component";
 import { AuthComponent } from "./auth/auth.component";
 import { SingleAppareilComponent } from "./single-appareil/single-appareil.component";
+import { FourOhFourComponent } from "./four-oh-four/four-oh-four.component";
+import { AuthGuard } from "./services/auth-guard.service";
 
 const routes: Routes = [
-  { path: "appareils", component: AppareilViewComponent },
-  { path: "appareils/:id", component: SingleAppareilComponent },
+  {
+    path: "appareils",
+    canActivate: [AuthGuard],
+    component: AppareilViewComponent
+  },
+  {
+    path: "appareils/:id",
+    canActivate: [AuthGuard],
+    component: SingleAppareilComponent
+  },
   { path: "auth", component: AuthComponent },
-  { path: "", component: AppareilViewComponent }
+  { path: "", component: AppareilViewComponent },
+  { path: "not-found", component: FourOhFourComponent },
+  { path: "**", redirectTo: "/not-found" }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { AuthComponent } from "./auth/auth.component";
 import { AppareilViewComponent } from "./appareil-view/appareil-view.component";
 import { AuthService } from "./services/auth.service";
 import { SingleAppareilComponent } from "./single-appareil/single-appareil.component";
+import { FourOhFourComponent } from "./four-oh-four/four-oh-four.component";
+import { AuthGuard } from "./services/auth-guard.service";
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,10 +21,11 @@ import { SingleAppareilComponent } from "./single-appareil/single-appareil.compo
     AppareilComponent,
     AuthComponent,
     AppareilViewComponent,
-    SingleAppareilComponent
+    SingleAppareilComponent,
+    FourOhFourComponent
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [AppareilService, AuthService],
+  providers: [AppareilService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
